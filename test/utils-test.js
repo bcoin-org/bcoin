cd3f04268af8b814a58e1b484c3f202b800912ac
Fedor Indutny <fedor@indutny.com>
2014-05-03 16:51:16 +0400

    block: verify

--- test/utils-test.js
+++ test/utils-test.js
@@ -8,4 +8,14 @@ describe('Utils', function() {
     assert.equal(b, '1116h8cQN');
     assert.deepEqual(bcoin.utils.fromBase58(b), arr);
   });
+
+  it('should translate bits to target', function() {
+    var bits = 0x1900896c;
+    var hash = bcoin.utils.toArray(
+      '672b3f1bb11a994267ea4171069ba0aa4448a840f38e8f340000000000000000',
+      'hex'
+    );
+    var target = bcoin.utils.bitsToTarget(bits);
+    assert(bcoin.utils.testTarget(target, hash));
+  });
 });
