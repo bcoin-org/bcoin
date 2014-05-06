69f3e85e37890a5213560e361873797a43ffff26
Fedor Indutny <fedor@indutny.com>
2014-05-06 16:25:46 +0400

    tx-pool: initial

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -70,4 +70,28 @@ describe('Wallet', function() {
     w.sign(tx);
     assert(tx.verify());
   });
+
+  it('should have TX pool', function() {
+    var w = bcoin.wallet();
+
+    // Coinbase
+    var t1 = bcoin.tx().out(w, 50000).out(w, 1000);
+    var t2 = bcoin.tx().input(t1.hash(), 0)
+                       .out(w, 24000)
+                       .out(w, 24000);
+    var t3 = bcoin.tx().input(t1.hash(), 1)
+                       .input(t2.hash(), 0)
+                       .out(w, 23000);
+    var t4 = bcoin.tx().input(t2.hash(), 1)
+                       .input(t3.hash(), 0)
+                       .out(w, 22000);
+    w.addTX(t4);
+    assert.equal(w.balance().toString(10), '22000');
+    w.addTX(t1);
+    assert.equal(w.balance().toString(10), '73000');
+    w.addTX(t2);
+    assert.equal(w.balance().toString(10), '47000');
+    w.addTX(t3);
+    assert.equal(w.balance().toString(10), '22000');
+  });
 });
