51b2e1f9a9fc436363bbd748a7c656d7641cdb64
Christopher Jeffrey <chjjeffrey@gmail.com>
2016-03-06 00:57:18 -0800

    improve sync. misc fixes.

--- test/protocol-test.js
+++ test/protocol-test.js
@@ -72,13 +72,13 @@ describe('Protocol', function() {
     assert.equal(payload.length, 2);
 
     assert.equal(typeof payload[0].ts, 'number');
-    assert.equal(payload[0].services, 1);
+    assert.equal(payload[0].services, bcoin.protocol.constants.bcoinServices);
     assert.equal(payload[0].ipv6, peers[0]._ipv6);
     assert.equal(payload[0].ipv4, peers[0]._ipv4);
     assert.equal(payload[0].port, peers[0].port);
 
     assert.equal(typeof payload[1].ts, 'number');
-    assert.equal(payload[1].services, 1);
+    assert.equal(payload[1].services, bcoin.protocol.constants.bcoinServices);
     assert.equal(payload[1].ipv6, peers[1]._ipv6);
     assert.equal(payload[1].ipv4, peers[1]._ipv4);
     assert.equal(payload[1].port, peers[1].port);
