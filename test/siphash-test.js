22e90303a254a69d149367b9dec16bd1071ba877
Christopher Jeffrey <chjjeffrey@gmail.com>
2016-08-25 14:47:50 -0700

    siphash: refactor.

--- test/siphash-test.js
+++ test/siphash-test.js
@@ -2,41 +2,26 @@
 
 var assert = require('assert');
 var siphash = require('../lib/crypto/siphash');
+var U64 = siphash.U64;
 
 describe('SipHash', function() {
   it('should perform siphash with no data', function() {
-    var k0 = new Buffer(8);
-    var k1 = new Buffer(8);
-    siphash.write(k0, { hi: 0x07060504, lo: 0x03020100 }, 0);
-    siphash.write(k1, { hi: 0x0F0E0D0C, lo: 0x0B0A0908 }, 0);
-    // be:
-    // assert.equal(siphash(k0, k1, new Buffer(0)).toString('hex'), '726fdb47dd0e0e31');
-    // le:
+    var k0 = U64(0x07060504, 0x03020100).toRaw();
+    var k1 = U64(0x0f0e0d0c, 0x0b0a0908).toRaw();
     assert.equal(siphash(new Buffer(0), k0, k1).toString('hex'), '310e0edd47db6f72');
   });
 
   it('should perform siphash with data', function() {
-    var k0 = new Buffer(8);
-    var k1 = new Buffer(8);
-    var data = new Buffer(8);
-    siphash.write(k0, { hi: 0x07060504, lo: 0x03020100 }, 0);
-    siphash.write(k1, { hi: 0x0F0E0D0C, lo: 0x0B0A0908 }, 0);
-    siphash.write(data, { hi: 0x07060504, lo: 0x03020100 }, 0);
-    // be:
-    // assert.equal(siphash(k0, k1, data).toString('hex'), '93f5f5799a932462');
-    // le:
+    var k0 = U64(0x07060504, 0x03020100).toRaw();
+    var k1 = U64(0x0f0e0d0c, 0x0b0a0908).toRaw();
+    var data = U64(0x07060504, 0x03020100).toRaw();
     assert.equal(siphash(data, k0, k1).toString('hex'), '6224939a79f5f593');
   });
 
   it('should perform siphash with uint256', function() {
-    var k0 = new Buffer(8);
-    var k1 = new Buffer(8);
-    siphash.write(k0, { hi: 0x07060504, lo: 0x03020100 }, 0);
-    siphash.write(k1, { hi: 0x0F0E0D0C, lo: 0x0B0A0908 }, 0);
+    var k0 = U64(0x07060504, 0x03020100).toRaw();
+    var k1 = U64(0x0f0e0d0c, 0x0b0a0908).toRaw();
     var hash = new Buffer('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f', 'hex');
-    // be:
-    // assert.equal(siphash(k0, k1, hash).toString('hex'), '7127512f72f27cce');
-    // le:
     assert.equal(siphash(hash, k0, k1).toString('hex'), 'ce7cf2722f512771');
   });
 });
