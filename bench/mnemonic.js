8a743f81bbd6a7ff4f85f58ceafd48f8bc4057d5
Christopher Jeffrey <chjjeffrey@gmail.com>
2016-09-13 20:07:15 -0700

    bench: add mnemonic benchmark.

---
+++ bench/mnemonic.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var bn = require('bn.js');
+var bcoin = require('../').set('main');
+var constants = bcoin.constants;
+var utils = bcoin.utils;
+var assert = require('assert');
+var bench = require('./bench');
+var Mnemonic = bcoin.hd.Mnemonic;
+
+var key = bcoin.hd.fromMnemonic();
+var phrase = key.mnemonic.getPhrase();
+
+assert.equal(Mnemonic.fromPhrase(phrase).getPhrase(), phrase);
+
+var end = bench('fromPhrase');
+for (var i = 0; i < 10000; i++)
+  Mnemonic.fromPhrase(phrase);
+end(i);
