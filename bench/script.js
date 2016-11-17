d3da8bbfb86fa06dc2c44d21a6c9c27d27f2ef52
Christopher Jeffrey <chjjeffrey@gmail.com>
2016-11-17 04:53:10 -0800

    script: optimize fromPubkeyhash and fromScripthash.

---
+++ bench/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var BN = require('bn.js');
+var bcoin = require('../').set('main');
+var constants = bcoin.constants;
+var utils = bcoin.utils;
+var assert = require('assert');
+var scriptTypes = constants.scriptTypes;
+var opcodes = constants.opcodes;
+var bench = require('./bench');
+var fs = require('fs');
+var Script = bcoin.script;
+
+bcoin.cache();
+
+Script.prototype.fromPubkeyhashOld = function fromScripthash(hash) {
+  assert(Buffer.isBuffer(hash) && hash.length === 20);
+  this.push(opcodes.OP_DUP);
+  this.push(opcodes.OP_HASH160);
+  this.push(hash);
+  this.push(opcodes.OP_EQUALVERIFY);
+  this.push(opcodes.OP_CHECKSIG);
+  this.compile();
+  return this;
+};
+
+Script.fromPubkeyhashOld = function fromScripthash(hash) {
+  return new Script().fromPubkeyhashOld(hash);
+};
+
+var hashes = [];
+for (var i = 0; i < 100000; i++)
+  hashes.push(bcoin.crypto.randomBytes(20));
+
+var end = bench('old');
+for (var i = 0; i < hashes.length; i++)
+  Script.fromPubkeyhashOld(hashes[i]);
+end(i);
+
+var end = bench('hash');
+for (var i = 0; i < hashes.length; i++)
+  Script.fromPubkeyhash(hashes[i]);
+end(i);
