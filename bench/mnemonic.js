aa7e550f91029dd5ef682c37f0434a1e72370a0f
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-03-11 23:04:31 -0800

    hd: do not store mnemonic on hd private key.

--- bench/mnemonic.js
+++ bench/mnemonic.js
@@ -5,8 +5,9 @@ var bench = require('./bench');
 var HD = require('../lib/hd');
 var Mnemonic = require('../lib/hd/mnemonic');
 
-var key = HD.fromMnemonic();
-var phrase = key.mnemonic.getPhrase();
+var mnemonic = new Mnemonic();
+var key = HD.fromMnemonic(mnemonic);
+var phrase = mnemonic.getPhrase();
 var i, end;
 
 assert.equal(Mnemonic.fromPhrase(phrase).getPhrase(), phrase);
