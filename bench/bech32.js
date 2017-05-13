83060ee964c32db05c66207aa88a0543508fa901
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-05-13 13:46:43 -0700

    bech32: rewrite and improve perf.

---
+++ bench/bech32.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Address = require('../lib/primitives/address');
+var crypto = require('../lib/crypto/crypto');
+var bench = require('./bench');
+
+var i, end, addr;
+
+var addrs = [];
+
+end = bench('serialize');
+for (i = 0; i < 10000; i++) {
+  addr = Address.fromProgram(0, crypto.randomBytes(20));
+  addrs.push(addr.toBech32());
+}
+end(i);
+
+end = bench('parse');
+for (i = 0; i < 10000; i++) {
+  addr = addrs[i];
+  addr = Address.fromBech32(addr);
+  addrs[i] = addr;
+}
+end(i);
