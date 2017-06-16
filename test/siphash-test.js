015fb8b0b6561d5c35c37fb37a63f9c91c7fc9cc
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-06-16 14:28:59 -0700

    siphash: return objects instead of buffers.

--- test/siphash-test.js
+++ test/siphash-test.js
@@ -2,14 +2,22 @@
 
 var assert = require('assert');
 var siphash = require('../lib/crypto/siphash');
+var siphash256 = siphash.siphash256;
 var U64 = siphash.U64;
+
+function toRaw(num) {
+  var data = Buffer.allocUnsafe(8);
+  data.writeUInt32LE(num.lo >>> 0, 0, true);
+  data.writeUInt32LE(num.hi >>> 0, 4, true);
+  return data;
+};
 
 describe('SipHash', function() {
   it('should perform siphash with no data', function() {
     var k0 = U64(0x07060504, 0x03020100).toRaw();
     var k1 = U64(0x0f0e0d0c, 0x0b0a0908).toRaw();
     var key = Buffer.concat([k0, k1]);
-    assert.equal(siphash(Buffer.alloc(0), key).toString('hex'), '310e0edd47db6f72');
+    assert.equal(toRaw(siphash256(Buffer.alloc(0), key)).toString('hex'), '310e0edd47db6f72');
   });
 
   it('should perform siphash with data', function() {
@@ -17,7 +25,7 @@ describe('SipHash', function() {
     var k1 = U64(0x0f0e0d0c, 0x0b0a0908).toRaw();
     var data = U64(0x07060504, 0x03020100).toRaw();
     var key = Buffer.concat([k0, k1]);
-    assert.equal(siphash(data, key).toString('hex'), '6224939a79f5f593');
+    assert.equal(toRaw(siphash256(data, key)).toString('hex'), '6224939a79f5f593');
   });
 
   it('should perform siphash with uint256', function() {
@@ -25,6 +33,6 @@ describe('SipHash', function() {
     var k1 = U64(0x0f0e0d0c, 0x0b0a0908).toRaw();
     var hash = Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f', 'hex');
     var key = Buffer.concat([k0, k1]);
-    assert.equal(siphash(hash, key).toString('hex'), 'ce7cf2722f512771');
+    assert.equal(toRaw(siphash256(hash, key)).toString('hex'), 'ce7cf2722f512771');
   });
 });
