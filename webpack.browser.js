307e7dd6982f1b8c0f83814535483894ec74f372
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-06-28 18:00:01 -0700

    webpack: get bcoin building in webpack for node.

--- webpack.browser.js
+++ webpack.browser.js
@@ -2,6 +2,8 @@
 
 var webpack = require('webpack')
 var path = require('path');
+var str = JSON.stringify;
+var env = process.env;
 
 module.exports = {
   target: 'web',
@@ -10,7 +12,7 @@ module.exports = {
     'bcoin-master': './lib/workers/master'
   },
   output: {
-    path: path.resolve(__dirname, 'browser'),
+    path: path.join(__dirname, 'browser'),
     filename: '[name].js'
   },
   resolve: {
@@ -20,11 +22,21 @@ module.exports = {
   module: {
     rules: [{
       test: /\.js$/,
-      exclude: path.resolve(__dirname, 'node_modules'),
+      exclude: path.join(__dirname, 'node_modules'),
       loader: 'babel-loader'
     }]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.BCOIN_NETWORK':
+        str(env.BCOIN_NETWORK || 'main'),
+      'process.env.BCOIN_WORKERS_ENABLED':
+        str(env.BCOIN_WORKERS_ENABLED || '0'),
+      'process.env.BCOIN_WORKER_URL':
+        str(env.BCOIN_WORKER_URL || '/bcoin-worker.js'),
+      'process.env.BCOIN_MASTER_URL':
+        str(env.BCOIN_MASTER_URL || '/bcoin-master.js')
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
---
+++ webpack.node.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var webpack = require('webpack')
+var path = require('path');
+var str = JSON.stringify;
+var env = process.env;
+
+module.exports = {
+  target: 'node',
+  entry: {
+    'bcoin': './lib/bcoin-browser',
+    'bcoin-worker': './lib/workers/worker'
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.node', '.js', '.json'],
+    alias: {
+      'bindings': __dirname + '/webpack/bindings.js'
+    }
+  },
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    rules: [{
+      test: /\.node$/,
+      loader: 'node-loader'
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.BCOIN_WORKER_FILE':
+        str(env.BCOIN_WORKER_FILE || "path.join(__dirname, 'bcoin-worker.js')")
+    })
+  ]
+};
