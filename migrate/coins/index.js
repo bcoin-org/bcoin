91b994a349a85e1c5f948a9491f48cb4b544804a
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-07-03 01:27:40 -0700

    migrate: add pertxout migration.

---
+++ migrate/coins/index.js
@@ -0,0 +1,16 @@
+/*!
+ * coins/index.js - utxo management for bcoin
+ * Copyright (c) 2016-2017, Christopher Jeffrey (MIT License).
+ * https://github.com/bcoin-org/bcoin
+ */
+
+'use strict';
+
+/**
+ * @module coins
+ */
+
+exports.Coins = require('../../lib/coins/coins');
+exports.CoinView = require('../../lib/coins/coinview');
+exports.compress = require('../../lib/coins/compress');
+exports.UndoCoins = require('../../lib/coins/undocoins');
