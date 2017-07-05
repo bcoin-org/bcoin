lif-zone/lifcoin

coinview: refactor api.

lib/blockchain/chain.js
@@ -2325,19 +2325,19 @@ Chain.prototype.getLocks = async function getLocks(prev, tx, view, flags) {
   if (tx.isCoinbase() || tx.version < 2 || !hasFlag)
     return [minHeight, minTime];
 
-  for (let input of tx.inputs) {
+  for (let {prevout, sequence} of tx.inputs) {
     let height, time, entry;
 
-    if (input.sequence & disableFlag)
+    if (sequence & disableFlag)
       continue;
 
-    height = view.getHeight(input);
+    height = view.getHeight(prevout);
 
     if (height === -1)
       height = this.height + 1;
 
-    if ((input.sequence & typeFlag) === 0) {
-      height += (input.sequence & mask) - 1;
+    if ((sequence & typeFlag) === 0) {
+      height += (sequence & mask) - 1;
       minHeight = Math.max(minHeight, height);
       continue;
     }
@@ -2347,7 +2347,7 @@ Chain.prototype.getLocks = async function getLocks(prev, tx, view, flags) {
     assert(entry, 'Database is corrupt.');
 
     time = await entry.getMedianTime();
-    time += ((input.sequence & mask) << granularity) - 1;
+    time += ((sequence & mask) << granularity) - 1;
     minTime = Math.max(minTime, time);
   }
 

bcoin-org/bcoin

lib/blockchain/chain.js
@@ -2264,26 +2264,25 @@ Chain.prototype.getLocks = async function getLocks(prev, tx, view, flags) {
   let disableFlag = consensus.SEQUENCE_DISABLE_FLAG;
   let typeFlag = consensus.SEQUENCE_TYPE_FLAG;
   let hasFlag = flags & common.lockFlags.VERIFY_SEQUENCE;
-  let nextHeight = this.height + 1;
   let minHeight = -1;
   let minTime = -1;
 
   if (tx.isCoinbase() || tx.version < 2 || !hasFlag)
     return [minHeight, minTime];
 
-  for (let input of tx.inputs) {
+  for (let {prevout, sequence} of tx.inputs) {
     let coinHeight, coinTime, entry;
 
-    if (input.sequence & disableFlag)
+    if (sequence & disableFlag)
       continue;
 
-    coinHeight = view.getHeight(input);
+    coinHeight = view.getHeight(prevout);
 
     if (coinHeight === -1)
-      coinHeight = nextHeight;
+      coinHeight = this.height + 1;
 
-    if ((input.sequence & typeFlag) === 0) {
-      coinHeight += (input.sequence & mask) - 1;
+    if ((sequence & typeFlag) === 0) {
+      coinHeight += (sequence & mask) - 1;
       minHeight = Math.max(minHeight, coinHeight);
       continue;
     }
@@ -2292,7 +2291,7 @@ Chain.prototype.getLocks = async function getLocks(prev, tx, view, flags) {
     assert(entry, 'Database is corrupt.');
 
     coinTime = await entry.getMedianTime();
-    coinTime += ((input.sequence & mask) << granularity) - 1;
+    coinTime += ((sequence & mask) << granularity) - 1;
     minTime = Math.max(minTime, coinTime);
   }
 