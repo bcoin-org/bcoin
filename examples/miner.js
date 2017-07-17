ffc7a9bd6f848027d9bc621fa59d2177693b0b03
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-07-17 16:26:47 -0700

    examples: fix miner example.

--- examples/miner.js
+++ examples/miner.js
@@ -1,16 +1,26 @@
 'use strict';
 
+const KeyRing = require('bcoin/lib/primitives/keyring');
+const WorkerPool = require('bcoin/lib/workers/workerpool');
 const Chain = require('bcoin/lib/blockchain/chain');
 const Miner = require('bcoin/lib/mining/miner');
 
+const key = KeyRing.generate('regtest');
+
+const workers = new WorkerPool({
+  enabled: true
+});
+
 const chain = new Chain({
-  network: 'regtest'
+  network: 'regtest',
+  workers: workers
 });
 
 const miner = new Miner({
   chain: chain,
-  addresses: ['mfWxJ45yp2SFn7UciZyNpvDKrzbhyfKrY8'],
-  coinbaseFlags: 'my-miner'
+  addresses: [key.getAddress()],
+  coinbaseFlags: 'my-miner',
+  workers: workers
 });
 
 async function main() {
@@ -28,6 +38,7 @@ async function main() {
 
   console.log('Mined block:');
   console.log(block);
+  console.log(block.txs[0]);
 
   await chain.add(block);
 
