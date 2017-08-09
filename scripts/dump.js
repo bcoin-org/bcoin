7d4d2e6c7c75d75fc925d7e67a53c655326fa76c
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-08-09 15:26:54 -0700

    scripts: fix dump script.

--- scripts/dump.js
+++ scripts/dump.js
@@ -1,39 +1,21 @@
 'use strict';
 
-const fs = require('fs');
 const heapdump = require('heapdump');
+const fs = require('../lib/utils/fs');
 const MempoolEntry = require('../lib/mempool/mempoolentry');
 const Coins = require('../lib/coins/coins');
 const TX = require('../lib/primitives/tx');
-const CoinView = require('../lib/coins/coinview');
+const common = require('../test/util/common');
 
 const SNAPSHOT = `${__dirname}/../dump.heapsnapshot`;
-const tx = parseTX('../test/data/tx4.hex');
 
-function parseTX(file) {
-  const data = fs.readFileSync(`${__dirname}/${file}`, 'utf8');
-  const parts = data.trim().split(/\n+/);
-  let raw = parts[0];
-  const tx = TX.fromRaw(raw.trim(), 'hex');
-  const view = new CoinView();
-  let i, prev;
-
-  for (i = 1; i < parts.length; i++) {
-    raw = parts[i];
-    prev = TX.fromRaw(raw.trim(), 'hex');
-    view.addTX(prev, -1);
-  }
-
-  return { tx: tx, view: view };
-}
-
-const raw = Coins.fromTX(tx.tx, 0).toRaw();
-const coins = Coins.fromRaw(raw, tx.tx.hash('hex'));
-const entry = MempoolEntry.fromTX(tx.tx, tx.view, 1000000);
+const {tx, view} = common.parseTX('data/tx4.hex');
+const coins = Coins.fromTX(tx, 0);
+const entry = MempoolEntry.fromTX(tx, view, 1000000);
 
 setInterval(() => {
   console.log(tx.hash('hex'));
-  console.log(coins.hash);
+  console.log(coins.outputs.length);
   console.log(entry.tx);
 }, 60 * 1000);
 
