a6490bd7b14e5c94a793868fd6d04a1700802e01
bucko13 <bucko.perley@gmail.com>
2018-06-06 00:28:14 -0700

    setup mtx methods and initial wallet.sweep. prototyping tests

---
+++ test/mtx-test.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+const { MTX, Selector } = require('../lib/primitives/mtx');
+
+describe('MTX', () => {
+  it('should fill with coins', () => {
+
+  });
+
+  it('shouldn\'t fill if not enough coins', () => {
+
+  });
+});
+
+describe('CoinSelector', () => {
+  it('should select fill coins', () => {
+
+  });
+});
