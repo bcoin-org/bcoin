b076ee972d5d7fbedec46a7baad8b10290b8b8b5
Matthew Zipkin <pinheadmz@gmail.com>
2018-07-05 17:05:58 +0000

    test/txmeta: fix lint

--- test/txmeta-test.js
+++ test/txmeta-test.js
@@ -9,18 +9,17 @@ const TXMeta = require('../lib/primitives/txmeta');
 
 const network = Network.get('regtest');
 
-
 describe('TXMeta', function() {
   it('should return JSON for txmeta', async () => {
     // unconfirmed at height 100
     const txmeta1 = new TXMeta();
-    const txJSON1 = txmeta1.getJSON(network, null, 100)
+    const txJSON1 = txmeta1.getJSON(network, null, 100);
     assert.strictEqual(txJSON1.confirmations, 0);
 
     // confirmed once at height 100
     const txmeta2 = TXMeta.fromOptions( {height: 100} );
     txmeta2.height = 100;
-    const txJSON2 = txmeta2.getJSON(network, null, 100)
+    const txJSON2 = txmeta2.getJSON(network, null, 100);
     assert.strictEqual(txJSON2.confirmations, 1);
   });
 });
