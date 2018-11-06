1def77143bdfae4d7ea660a076ed399d866d1b5e
Braydon Fuller <courier@braydon.com>
2018-11-05 19:38:06 -0800

    wallet: add indexing for unconfirmed count and time

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -1314,19 +1314,15 @@ describe('Wallet', function() {
     });
   }
 
-  it('should get range of txs', async () => {
+  it('should get pending range of txs', async () => {
     const wallet = currentWallet;
-    const txs = await wallet.getRange(null, {
-      start: util.now() - 1000
-    });
+    const txs = await wallet.getPending(null);
     assert.strictEqual(txs.length, 2);
   });
 
-  it('should get range of txs from account', async () => {
+  it('should get pending range of txs from account', async () => {
     const wallet = currentWallet;
-    const txs = await wallet.getRange('foo', {
-      start: util.now() - 1000
-    });
+    const txs = await wallet.getPending('foo');
     assert.strictEqual(txs.length, 2);
   });
 
@@ -1457,9 +1453,7 @@ describe('Wallet', function() {
   it('should get details', async () => {
     const wallet = currentWallet;
 
-    const txs = await wallet.getRange('foo', {
-      start: util.now() - 1000
-    });
+    const txs = await wallet.getPending('foo');
 
     const details = await wallet.toDetails(txs);
 
@@ -1473,9 +1467,7 @@ describe('Wallet', function() {
 
     await wallet.rename('test');
 
-    const txs = await wallet.getRange('foo', {
-      start: util.now() - 1000
-    });
+    const txs = await wallet.getPending('foo');
 
     const details = await wallet.toDetails(txs);
 
