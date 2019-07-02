df3e55ce479f979e7d97f805c4b9ca12d67ff074
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2019-07-02 17:53:44 +0400

    rpc-test: add help rpc tests.

--- test/node-rpc-test.js
+++ test/node-rpc-test.js
@@ -45,14 +45,79 @@ describe('RPC', function() {
     await node.close();
   });
 
-  it('should rpc help', async () => {
-    assert(await nclient.execute('help', []));
+  describe('help', function() {
+    it('should list all methods', async () => {
+      const response = await nclient.execute('help', []);
+      const lines = response.split('\n');
 
-    await assert.rejects(async () => {
-      await nclient.execute('help', ['getinfo']);
-    }, {
-      name: 'Error',
-      message: /^getinfo/
+      assert(response);
+      assert(lines.length);
+
+      assert.strictEqual(lines[0], 'Select a command:');
+    });
+
+    it('should contain node rpc method', async () => {
+      const response = await nclient.execute('help', []);
+      const lines = response.split('\n');
+
+      assert(response);
+      assert(lines.length);
+
+      // methods that are less likely to be depracated soon.
+      assert(lines.includes('getblockchaininfo'));
+
+      // server methods
+      assert(lines.includes('help'));
+      assert(lines.includes('stop'));
+    });
+
+    it('should not contain wallet rpc methods', async () => {
+      const response = await nclient.execute('help', []);
+      const lines = response.split('\n');
+
+      assert(response);
+      assert(lines.length);
+
+      // methods that are less likely to be depracated soon.
+      assert(!lines.includes('getwalletinfo'));
+    });
+
+    it('should get method help', async () => {
+      await assert.rejects(async () => {
+        await nclient.execute('help', ['getblockchaininfo']);
+      }, {
+        name: 'Error',
+        message: 'getblockchaininfo'
+      });
+
+      await assert.rejects(async () => {
+        await nclient.execute('help', ['help']);
+      }, {
+        name: 'Error',
+        type: 'RPCError',
+        message: 'help ( "command" )'
+      });
+    });
+
+    it('should return error on wrong command', async () => {
+      const wrongCommand = 'wrongcommand';
+
+      await assert.rejects(async () => {
+        await nclient.execute('help', [wrongCommand]);
+      }, {
+        name: 'Error',
+        message: `Method not found: ${wrongCommand}.`
+      });
+    });
+
+    it('should return help on wrong command', async () => {
+      await assert.rejects(async () => {
+        await nclient.execute('help', ['command1', 'command2']);
+      }, {
+        name: 'Error',
+        type: 'RPCError',
+        message: 'help ( "command" )'
+      });
     });
   });
 
