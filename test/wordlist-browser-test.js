407b1fd1064e858fb0fcea3fd51803796976c376
Johnathan James <haxwell@gmail.com>
2023-01-08 23:32:04 -0700

    Wordlist-browser-test, corrected lint errors.

--- test/wordlist-browser-test.js
+++ test/wordlist-browser-test.js
@@ -4,22 +4,22 @@
 'use strict';
 
 const assert = require('bsert');
-const wordlist_browser = require('../lib/hd/wordlist-browser');
+const wordlistBrowser = require('../lib/hd/wordlist-browser');
 const words = require('../lib/hd/words');
 
 describe('wordlist-browser', function() {
   it('should return the appropriate wordlist', () => {
-    assert.strictEqual(wordlist_browser.get(`simplified chinese`), words.chinese.simplified);
-    assert.strictEqual(wordlist_browser.get(`traditional chinese`), words.chinese.traditional);
-    assert.strictEqual(wordlist_browser.get(`english`), words.english);
-    assert.strictEqual(wordlist_browser.get(`french`), words.french);
-    assert.strictEqual(wordlist_browser.get(`italian`), words.italian);
-    assert.strictEqual(wordlist_browser.get(`japanese`), words.japanese);
-    assert.strictEqual(wordlist_browser.get(`spanish`), words.spanish);
+    assert.strictEqual(wordlistBrowser.get('simplified chinese'), words.chinese.simplified);
+    assert.strictEqual(wordlistBrowser.get('traditional chinese'), words.chinese.traditional);
+    assert.strictEqual(wordlistBrowser.get('english'), words.english);
+    assert.strictEqual(wordlistBrowser.get('french'), words.french);
+    assert.strictEqual(wordlistBrowser.get('italian'), words.italian);
+    assert.strictEqual(wordlistBrowser.get('japanese'), words.japanese);
+    assert.strictEqual(wordlistBrowser.get('spanish'), words.spanish);
 
     // expect error when language is not found
     assert.throws(() => {
-      wordlist_browser.get(`not a language`);
+      wordlistBrowser.get(`not a language`);
     }, Error, `Unknown language: not a language.`);
   });
 });
